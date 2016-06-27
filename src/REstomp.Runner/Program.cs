a406e4f4ec121e51a5ac45d54d5794dbec80525a
Ovan Crone <ovan@psibr.net>
2016-06-27 15:47:34 -0500

    Adding the code for the runner.

--- src/REstomp.Runner/Program.cs
+++ src/REstomp.Runner/Program.cs
@@ -6,7 +6,14 @@ namespace REstomp.Runner
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using(var reStompService = new REstomp.StompService(System.Net.IPAddress.Parse("127.0.0.1"), 5467))
+            {
+                reStompService.Start();
+
+                Console.WriteLine("Service started.");
+
+                Console.ReadLine();
+            }
         }
     }
 }
