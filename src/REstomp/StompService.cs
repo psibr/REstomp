ec0bc5c1f3474686713fa378a4c41a5089d0d55b
Ovan Crone <ovan@psibr.net>
2016-06-28 12:34:55 -0500

    Converting to Visual Studio compatible mode.

--- src/REstomp/StompService.cs
+++ src/REstomp/StompService.cs
@@ -10,11 +10,6 @@ using System.Threading.Tasks;
 
 namespace REstomp
 {
-    public class ConsumerServiceConfiguration
-    {
-        public string ApiKey { get; set; }
-    }
-
     public class StompService
         : IDisposable
     {
@@ -180,47 +175,59 @@ namespace REstomp
             Stream = stream;
         }
 
-        /// <summary>
-        /// Reads a STOMP command string asynchronyously.
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <returns>CommandString if read; otherwise null</returns>
-        public async Task<string> ReadStompCommand(CancellationToken cancellationToken)
+    /// <summary>
+    /// Reads a STOMP command string asynchronyously.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns>CommandString if read; otherwise null</returns>
+    public async Task<string> ReadStompCommand(CancellationToken cancellationToken)
+    {
+        string commandString = null;
+
+        //Create a new buffer, maximum possible allowed bytes in the line is 13
+        var commandBuffer = new byte[13]; 
+
+        //EOL is the line ending as defined in the spec of STOMP 1.2
+        //EOL can either be CRLF or LF alone
+        var eolIndex = -1;
+        var bytesRead = 0;
+
+        //while we haven't maxed out our buffer and we haven't found an EOL sequence
+        while (bytesRead < commandBuffer.Length && eolIndex == -1)
         {
-            string commandString = null;
+            //move forward in the buffer
+            var offset = bytesRead;
 
-            var commandBuffer = new byte[13];
-            var eolIndex = -1;
-            var bytesRead = 0;
-            while (bytesRead < commandBuffer.Length && eolIndex == -1)
+            //Reading 1 byte at a time for now, may change in a future implementation
+            const int length = 1;
+            var bytesFound = 0;
+
+            bytesFound += await Stream.ReadAsync(commandBuffer, offset, length, cancellationToken);
+
+            //check for EOL in the bytes we read. (1 iteration since length is const of 1)
+            for (var i = offset; i < offset + bytesFound; i++)
             {
-                var offset = bytesRead;
-                const int length = 1;
-                var bytesFound = 0;
-
-                bytesFound += await Stream.ReadAsync(commandBuffer, offset, length, cancellationToken);
-
-                for (var i = offset; i < offset + bytesFound; i++)
-                {
-                    if (commandBuffer[i] == 0x0a)
-                        eolIndex = i;
-                }
-
-                bytesRead += bytesFound;
+                if (commandBuffer[i] == 0x0a)
+                    eolIndex = i;
             }
 
-            if (eolIndex > 2)
-            {
-                //If last byte before LF was CR, move the EOL start index back one.
-                if (commandBuffer[eolIndex - 1] == 0x0d)
-                {
-                    eolIndex--;
-                }
-
-                commandString = Encoding.UTF8.GetString(commandBuffer, 0, eolIndex);
-            }
-
-            return commandString;
+            bytesRead += bytesFound;
         }
+
+        //if we have a potentially meaningful line (STOMPs shortest command is 3 characters)
+        if (eolIndex > 2)
+        {
+            //If last byte before LF was CR, move the EOL start index back one
+            if (commandBuffer[eolIndex - 1] == 0x0d)
+            {
+                eolIndex--;
+            }
+
+            //Convert bytes to string in UTF-8 from beginning to start of EOL
+            commandString = Encoding.UTF8.GetString(commandBuffer, 0, eolIndex);
+        }
+
+        return commandString;
+    }
     }
 }
--- test/REstomp.Test/Tests.cs
+++ test/REstomp.Test/Tests.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using System.Net;
 using Xunit;
 
-namespace REstomp.Tests
+namespace REstomp.Test
 {
     public class ParserTests
     {
         [Fact]
         public void Test1() 
         {
-            using(var reStompService = new REstomp.StompService(System.Net.IPAddress.Parse("127.0.0.1"), 5467))
+            using(var reStompService = new StompService(IPAddress.Parse("127.0.0.1"), 5467))
             {
                 reStompService.Start();
             }
