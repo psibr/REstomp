571a8bcf67ebeeed9b72e66ed972acf625252173
Ovan Crone <ovan@psibr.net>
2016-06-30 22:41:12 -0500

    Functional Changes in parser
    - Renaming CommandString to Command
    - Renaming StompStreamParser to StompParser
    - Adding StompParser.Commands
    - Adding a functional .With to StompFrame

--- src/REstomp/CommandStringParseException.cs
+++
@@ -1,9 +0,0 @@
-namespace REstomp
-{
-    public class CommandStringParseException : System.Exception
-    {
-        public CommandStringParseException() { }
-        public CommandStringParseException( string message ) : base( message ) { }
-        public CommandStringParseException( string message, System.Exception inner ) : base( message, inner ) { }
-    }
-}
--- src/REstomp/StompFrame.cs
+++ src/REstomp/StompFrame.cs
@@ -1,31 +1,73 @@
+using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq.Expressions;
 
 namespace REstomp
 {
     public class StompFrame
     {
-        public StompFrame(string commandString, ImmutableDictionary<string, string> headers, byte[] body)
+        public StompFrame(string command, IDictionary<string, string> headers, byte[] body)
         {
-            CommandString = commandString;
-            Headers = headers;
-            Body = body;
+            Command = command;
+            Headers = headers?.ToImmutableDictionary() ?? ImmutableDictionary<string, string>.Empty;
+            Body = body.ToImmutableArray();
         }
 
-        public StompFrame(string commandString, ImmutableDictionary<string, string> headers)
-            : this(commandString, headers, new byte[0])
+        public StompFrame(string command, IDictionary<string, string> headers, ImmutableArray<byte> body)
+        {
+            Command = command;
+            Headers = headers?.ToImmutableDictionary() ?? ImmutableDictionary<string, string>.Empty;
+            Body = body.ToImmutableArray();
+        }
+
+        public StompFrame(string command, IDictionary<string, string> headers)
+            : this(command, headers, new byte[0])
         {
         }
 
-        public StompFrame(string commandString)
-            : this(commandString, ImmutableDictionary<string, string>.Empty, new byte[0])
+        public StompFrame(string command)
+            : this(command, ImmutableDictionary<string, string>.Empty, new byte[0])
         {
         }
 
+        private StompFrame()
+        { }
 
-        public string CommandString { get; }
+
+        public string Command { get; }
 
         public ImmutableDictionary<string, string> Headers { get; }
 
-        public byte[] Body { get; }
+        public ImmutableArray<byte> Body { get; }
+
+        public static StompFrame Empty { get; } = new StompFrame();
+
+
+        public StompFrame With<TMember>(Expression<Func<StompFrame, TMember>> mutationSelectorExpression, TMember value)
+        {
+            var memberName = ((MemberExpression)mutationSelectorExpression.Body).Member.Name;
+            var commandString = Command;
+            var headers = Headers;
+            var body = Body;
+
+            var obj = (object)value;
+
+            switch (memberName)
+            {
+                case nameof(Command):
+                    commandString = (string)obj;
+                    break;
+                case nameof(Headers):
+                    headers = (ImmutableDictionary<string, string>)obj;
+                    break;
+                case nameof(Body):
+                    body = (ImmutableArray<byte>)obj;
+                    break;
+            }
+
+            return new StompFrame(commandString, headers, body);
+        }
+
     }
 }
