d8d08f2a44e4d8a1599558f127b97e773343ff14
Ovan Crone <ovan@psibr.net>
2016-07-04 01:19:05 -0500

    Adding PrependableStreams and extensions
    - Adding PrependableStream class
    - Adding an AsPrependableStream class

---
+++ src/REstomp/PrependableStreamExtensions.cs
@@ -0,0 +1,13 @@
+using System.IO;
+
+namespace REstomp
+{
+    public static class PrependableStreamExtensions
+    {
+        public static PrependableStream<TStream> AsPrependableStream<TStream>(this TStream stream)
+            where TStream : Stream
+        {
+            return new PrependableStream<TStream>(stream);
+        }
+    }
+}
